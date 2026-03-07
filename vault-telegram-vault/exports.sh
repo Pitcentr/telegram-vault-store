dd7fa405eff00f50a990a0d1617c9804a78f7194
Petr <pitfor@gmail.com>
2026-03-07 08:58:52 +0300

    fix

---
+++ vault-telegram-vault/exports.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This file is used by Umbrel to set environment variables
+# Users should configure these values before installation
+
+export APP_TG_TOKEN="${APP_TG_TOKEN:-}"
+export APP_PB_ADMIN="${APP_PB_ADMIN:-admin@vault.local}"
+export APP_PB_PASSWORD="${APP_PB_PASSWORD:-}"
+export APP_MASTER_PASSWORD="${APP_MASTER_PASSWORD:-}"
+export APP_ALLOWED_USERS="${APP_ALLOWED_USERS:-}"
